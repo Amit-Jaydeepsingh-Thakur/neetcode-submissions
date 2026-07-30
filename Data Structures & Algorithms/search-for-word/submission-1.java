1569397d565568ce2c3d1ef01c3b7269c294fa48
Amit Jaydeepsingh Thakur <thakuramitjaydeep@gmail.com>
2026-07-30 12:02:34 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int i = 0, j = 0;
+
+        for (i = 0; i < board.length; i++) {
+            for (j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (solve(i, j, 0, word, board)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean solve(int i, int j, int idx, String word, char[][] board) {
+
+        if (idx >= word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length
+            || board[i][j] == '#' || word.charAt(idx) != board[i][j]) {
+            return false;
+        };
+
+        
+
+        char org = board[i][j];
+        board[i][j] = '#';
+
+        boolean ret = solve(i + 1, j, idx + 1, word, board) ||
+                solve(i - 1, j, idx + 1, word, board) ||
+                solve(i, j + 1, idx + 1, word, board) ||
+                solve(i, j - 1, idx + 1, word, board);
+
+        board[i][j] = org;
+
+        return ret;
+
+
+    }
+}
